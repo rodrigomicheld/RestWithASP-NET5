2f89dabdf89f56a92be57717d7810dab6af1465c
Rodrigo Michel <rodrigo.meneses@ambevtech.com.br>
2022-05-21 23:32:20 -0300

    Implementando GET-POST-PUT-DELETE

---
+++ RestWithASPNET/RestWithASPNET/RestWithASPNET/Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNET.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindbyId(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);  
+
+    }
+}
--- RestWithASPNET/RestWithASPNET/RestWithASPNET/Startup.cs
+++ RestWithASPNET/RestWithASPNET/RestWithASPNET/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RestWithASPNET.Services;
+using RestWithASPNET.Services.Implementations;
 
 namespace RestWithASPNET
 {
@@ -27,6 +22,8 @@ namespace RestWithASPNET
         {
 
             services.AddControllers();
+
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
